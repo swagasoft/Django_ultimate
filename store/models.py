028c7ec9ea7a53fc60fcdb2a0af725c8565f8285
swagasoft <swagasoft@gmail.com>
2022-08-24 08:45:18 +0100

    initial commit

---
+++ store/models.py
@@ -0,0 +1,46 @@
+import email
+from django.db import models
+
+# Create your models here.
+class Products (models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+
+
+class Customer (models.Model):
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold')
+    ]
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=255)
+    birth_date = models.DateField(null=True)
+    membership = models.CharField(max_length=1, choices= MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+  
+class Order (models.Model):
+    PENDING_STATUS = 'P'
+    COMPLETE_STATUS = 'C'
+    FAILED_STATUS = 'F'
+
+    PAYMENT_STATUS_CHOICES = [
+        (PENDING_STATUS,'Pending'),
+        (COMPLETE_STATUS,'Complete'),
+        (FAILED_STATUS,'Failed')
+    ]
+    placed_at = models.DateField(auto_created=True)
+    payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default= PENDING_STATUS)
+
+class  Address (models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
