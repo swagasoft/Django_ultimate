74e2f8b7f658e1c749570942221aff99412e7f43
swagasoft <swagasoft@gmail.com>
2022-08-24 18:43:36 +0100

    added migrations

--- store/models.py
+++ store/models.py
@@ -1,13 +1,29 @@
 import email
+from pyexpat import model
+from turtle import title
 from django.db import models
 
-# Create your models here.
-class Products (models.Model):
+
+class Promotion (models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+
+
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
+
+class Product (models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    slug = models.SlugField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
 
 
 class Customer (models.Model):
@@ -27,6 +43,9 @@ class Customer (models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(max_length=1, choices= MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
   
+
+
+  
 class Order (models.Model):
     PENDING_STATUS = 'P'
     COMPLETE_STATUS = 'C'
@@ -39,8 +58,24 @@ class Order (models.Model):
     ]
     placed_at = models.DateField(auto_created=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS_CHOICES, default= PENDING_STATUS)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product,on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
 
 class  Address (models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
-    customer = models.OneToOneField(Customer, on_delete=models.CASCADE, primary_key=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Cart (models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItems (models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
+    quantity = models.PositiveSmallIntegerField()
