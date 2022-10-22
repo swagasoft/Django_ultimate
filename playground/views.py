181383e4c56276f6cb3d05bbcd2c8f6071860f0b
swagasoft <swagasoft@gmail.com>
2022-10-22 17:56:52 +0100

    added filtering to product

--- playground/views.py
+++ playground/views.py
@@ -1,7 +1,16 @@
 from unicodedata import name
 from django.shortcuts import render
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
+from store.models import Product
 
 # Create your views here.
 def say_hello(request):
-    return render(request, 'hello.html',{'name':'simon J'})
+  
+    # products = Product.objects.filter(unit_price__gt = 20)
+    queryset = Product.objects.filter(unit_price__range =(20, 30))
+    print('product ', products)
+
+    
+
+    return render(request, 'hello.html',{'name':'simon J','products':list(queryset)})
